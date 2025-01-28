cafa61cda7329677e8f7ec77e1142deecbb843f1
xa4os34 <matyushag8907@gmail.com>
2025-01-28 14:57:16 +0500

    Added error handling to SchedulePullingService.

--- src/rsp/SchedulePullingService.cs
+++ src/rsp/SchedulePullingService.cs
@@ -1,6 +1,8 @@
+using System.Net;
 using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace rsp;
 
@@ -8,18 +10,24 @@ public class SchedulePullingService : IHostedService
 {
     private readonly TimeSpan _pullingInterval = TimeSpan.FromMilliseconds(100);
     private readonly CancellationTokenSource _cts = new();
-    private readonly HttpClient _client = new();
+    private readonly HttpClient _client; 
     private readonly IMediator _mediator;
-    
+    private readonly ILogger<SchedulePullingService> _logger;
+
     private int _sequentialCode;
     private Task? _pullingTask;
 
     public SchedulePullingService(
+        ILogger<SchedulePullingService> logger,
         IConfiguration configuration,
         IMediator mediator) 
     {
+        _logger = logger;
         _sequentialCode = configuration.GetValue<int>("sequentialCode");
         _mediator = mediator;
+        _client = new HttpClient(new HttpClientHandler() {
+                AllowAutoRedirect = false
+            });
     }
     
     public Task StartAsync(CancellationToken cancellatinoToken)
@@ -46,16 +54,39 @@ public class SchedulePullingService : IHostedService
     {
         while (!cancellationToken.IsCancellationRequested)
         {
-            byte[] rawSchedule = await _client.GetByteArrayAsync(
-                $"https://permaviat.ru/_engine/get_file.php?f={_sequentialCode}&d=_res/fs/&p=file.pdf");
-
-            await _mediator.Publish<NewRawSchedule>(
-                new NewRawSchedule {
-                    Data = rawSchedule
-                });
-
-            _sequentialCode++;
             await Task.Delay(_pullingInterval, cancellationToken);
+
+            try 
+            {
+                HttpResponseMessage response = await _client.GetAsync(
+                    $"https://permaviat.ru/_engine/get_file.php?f={_sequentialCode}&d=_res/fs/&p=file.pdf",
+                    cancellationToken);
+
+                if (response.StatusCode is HttpStatusCode.Found)
+                    continue;
+
+                if (response.StatusCode is not HttpStatusCode.OK)
+                {
+                    _logger.LogWarning(
+                        "Unexpected http status code ({StatusCode}/{StatusCodeName}) ocured on downloading schedule.",
+                        response.StatusCode,
+                        Enum.GetName(response.StatusCode));
+                }
+
+                byte[] rawSchedule = await response.Content.ReadAsByteArrayAsync();
+
+                await _mediator.Publish<NewRawSchedule>(
+                    new NewRawSchedule {
+                        Data = rawSchedule
+                    });
+
+                _sequentialCode++;
+            }
+            catch (HttpRequestException ex) 
+            {
+                _logger.LogError(0, ex, 
+                    "Unexpected HttpRequestException ocured on downloading schedule, probably connectivity issue.");
+            }
         }
     }
 
